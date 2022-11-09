e3b6e041be8220463e1dba1da98f080af565e875
Enzo Giacalone <enzo.giacalone@etu.univ-poitiers.fr>
2022-11-09 11:11:47 +0000

    First version

---
+++ helloworld.scala
@@ -0,0 +1,19 @@
+//scala program to print Hello World !
+object Geeks
+{
+	// Main Method
+	def main(args:Array[String])
+	{
+		//prints Hello World
+		println("Hello World !")
+		var var3 = 2
+		if(var3 == 1) 
+			{
+			println("True")
+			}
+		else
+			{
+			println("False")
+			}
+	}	
+}
