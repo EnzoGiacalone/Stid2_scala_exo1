20e3ef16dc729df448a42b86ae459373ae16895b
Enzo Giacalone <enzo.giacalone@etu.univ-poitiers.fr>
2022-11-23 14:19:04 +0000

    version huit

--- helloworld.scala
+++ helloworld.scala
@@ -28,6 +28,9 @@ object Geeks
 		val chiffre3 : List[Int] = List(1,1,2,3,2,4,5)
 		println{chiffre3 map(x => (x, chiffre3.count(_ == x)))}
 
+		val chiffre4 : List[Int] = List(1,1,2,3,2,4,5)
+		println{chiffre4.groupBy(x => x).map(x=>(x._1, x._2.length))}
+
 		val lettre1 : List[String] = List("AA","BBB","C","DDDD")
 		println{lettre1 map(x => (x, x.length))} 
 
