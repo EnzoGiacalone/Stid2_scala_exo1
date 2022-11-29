822b6dc29464dcb18600fe264965f1aac10b2655
Enzo Giacalone <enzo.giacalone@etu.univ-poitiers.fr>
2022-11-29 15:01:37 +0000

    deuxième partie Etudiant

--- etudiant.scala
+++ etudiant.scala
@@ -16,6 +16,18 @@ class Etudiant(nomEtu:String,prenomEtu:String,ageEtu:Int,adresseEtu:String) {
     override def toString():String=nom+" "+prenom+" "+age+" "+adresse
 }
 
+class Note(numEtu:Int,nomModule:String,noteEtu:Double){
+    var num:Int=numEtu
+    var module:String=nomModule
+    var note:Double=noteEtu
+
+    def ChangeNote(newNote:Double){
+        note = newNote
+    }
+
+    def toNote():String=num+" "+module+" "+note
+}
+
 object STID2 {
     def main(args:Array[String]) {
         val etudiant1 = new Etudiant("Jean", "Philippe", 20,"15 rue de Niort,79000")
@@ -27,5 +39,14 @@ object STID2 {
         etudiant1.changeAdresse("14 rue de Poitiers");
         println(etudiant1.nom+" "+etudiant1.age)
         println(etudiant1.toString())
+   
+        val etudiant2 = new Etudiant("Enzo", "Giacalone", 20,"7 rue du Galuchet,79000")
+        etudiant2.changeAdresse("49 rue de Marseille");
+        println(etudiant2.toString())
+
+        val note1 = new Note(22008427, "Stats",20)
+        println(note1.toNote())
+        note1.ChangeNote(19)
+        println(note1.toNote())
     }
 }
